49139a51ff20e757aa3dbb038e467e22e06724b9
dekensruz <ruzubadekens@gmail.com>
2026-04-03 02:21:21 +0200

    Afficher un bool

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -7,3 +7,6 @@ Console.WriteLine('d');
 Console.WriteLine(123);
 Console.WriteLine(0.25F);
 Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
