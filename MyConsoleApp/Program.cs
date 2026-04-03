f57484fe3da6b3e2f510007d3814249b549c2e00
dekensruz <ruzubadekens@gmail.com>
2026-04-03 02:31:38 +0200

    Déclaration des variables

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -10,3 +10,22 @@ Console.WriteLine(2.625);
 Console.WriteLine(12.39816m);
 Console.WriteLine(true);
 Console.WriteLine(false);
+
+
+// Output: 123
+Console.WriteLine("123");
+// Output: 123
+Console.WriteLine(123);
+
+// Output: true
+Console.WriteLine("true");
+// Output: True
+Console.WriteLine(true);
+
+
+// DECLARATION DES VARIABLES
+
+string firstName;
+int age;
+decimal taille;
+bool estEtudiant;
