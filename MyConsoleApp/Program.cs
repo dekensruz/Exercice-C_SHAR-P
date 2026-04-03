ea5e27fb43a1845f0b39f732d80c7d210f21ca2a
dekensruz <ruzubadekens@gmail.com>
2026-04-03 02:18:49 +0200

    Afficher un entier et un décimal

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,3 +1,9 @@
-﻿Console.WriteLine("Hello world");
-Console.WriteLine("This is the first line");
+﻿// AFFICHER UN MESSAGE
+
+Console.WriteLine("Hello world");
+Console.Write("This is the first line");
 Console.WriteLine("This is the second line");
+Console.WriteLine('d');
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
